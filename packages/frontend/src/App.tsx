7a0f043f3c20271a2c2e991ddc43408d6451bdbe
Ivan <3891584+Thalandor@users.noreply.github.com>
2022-07-13 20:14:31 +0200

    Fix errors with new react router

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,22 +1,22 @@
-import './App.scss';
-import { Routes, Route } from 'react-router-dom';
-import Profile from './components/profile/Profile';
-import Feed from './components/feed/Feed';
-import Header from './components/header/Header';
-import EnsureLoggedIn from './components/login/EnsureLoggedIn';
-import Login from './components/login/Login';
-import Footer from './components/footer/Footer';
+import "./App.scss";
+import { Routes, Route } from "react-router-dom";
+import Profile from "./components/profile/Profile";
+import Feed from "./components/feed/Feed";
+import Header from "./components/header/Header";
+import EnsureLoggedIn from "./components/login/EnsureLoggedIn";
+import Login from "./components/login/Login";
+import Footer from "./components/footer/Footer";
 
 export enum AppRoutePath {
   Feed = "/",
   Profile = "/Profile",
-  Login = "/Login"
+  Login = "/Login",
 }
 
 const PrivateApp = () => {
   return (
     <div className="App">
-      <Route component={<EnsureLoggedIn />} />
+      <Route element={<EnsureLoggedIn />} />
       <header className="header">
         <Header></Header>
       </header>
@@ -32,17 +32,16 @@ const PrivateApp = () => {
       </main>
       <Footer></Footer>
     </div>
-  )
-}
+  );
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path={AppRoutePath.Login} element={Login}></Route>
-      <Route element={PrivateApp}></Route>
+      <Route path={AppRoutePath.Login} element={<Login />}></Route>
+      <Route element={<PrivateApp />}></Route>
     </Routes>
-
   );
-}
+};
 
 export default App;

af2b090a60bb5b906a2e7fbc3f6d076d1eb45e7e
IvanBV <3891584+Thalandor@users.noreply.github.com>
2022-07-21 23:15:22 +0200

    Update web3

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { HashRouter as Router } from 'react-router-dom';
-import axios from 'axios';
-import { Provider } from 'react-redux';
-import store from './redux/store/store';
+import React from "react";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { HashRouter as Router } from "react-router-dom";
+import axios from "axios";
+import { Provider } from "react-redux";
+import store from "./redux/store/store";
+import ReactDOM from "react-dom/client";
 
-axios.defaults.baseURL = 'http://localhost:3001';
+axios.defaults.baseURL = "http://localhost:3001";
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <Router>
       <Provider store={store}>
         <App />
       </Provider>
     </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
